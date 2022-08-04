a488deb820059019f9c35675059833e84044f90b
lebrancconvas <lebrancconvas@gmail.com>
2022-08-04 20:25:44 +0700

    🔧 feat: connect mongoDB with express

--- apps/server/src/config/config.ts
+++ apps/server/src/config/config.ts
@@ -6,7 +6,7 @@ dotenv.config();
 // Setup Mongo Username, Password, and URL. 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.jcrdy3g.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@mern-turbo-starter-clus.zjkp4my.mongodb.net/?retryWrites=true&w=majority`;
 
 // Setup Server Port. 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,13 +1,28 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes/router';
+import mongoose from 'mongoose';
+import { config } from './config/config';
 
 const app = express();
 const PORT = process.env.PORT || 3802;
 
 app.use(cors());
 app.use(express.json());
+
+// Using API Router. 
 app.use(router);
+
+// Connect MongoDB 
+mongoose
+	.connect(config.mongo.url)
+	.then(() => {
+		console.log('[SUCCESS] Connected to MongoDB.'); 
+	})
+	.catch(err => {
+		console.error(err);
+		console.log('[FAILURE] Can\'t connected to MongoDB.')
+	})
 
 app.listen(PORT, () => {
 	try {
