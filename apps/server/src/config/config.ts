0561d029015e3641309fb4eb1aa2b8df24d6591f
lebrancconvas <lebrancconvas@gmail.com>
2022-08-04 20:03:29 +0700

    🔧 feat: config mongoDB

---
+++ apps/server/src/config/config.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+
+// Use Config Data from .env file
+dotenv.config();
+
+// Setup Mongo Username, Password, and URL. 
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.jcrdy3g.mongodb.net/?retryWrites=true&w=majority`;
+
+// Setup Server Port. 
+const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
+
+// Config. 
+export const config = {
+	mongo: {
+		url: MONGO_URL
+	},
+	server: {
+		port: SERVER_PORT
+	}
+};
