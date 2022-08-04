dd4b7ec44e5235149e69cedc646fe402108fb19c
lebrancconvas <lebrancconvas@gmail.com>
2022-08-04 20:40:04 +0700

    💄 style: update web UI to be the default style

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return(
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
